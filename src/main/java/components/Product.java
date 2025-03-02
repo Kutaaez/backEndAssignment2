074e726f256ee8b3e869d8899f21e2ecc09d3e24
Kutaez <m_jasik_2005@mail.ru>
2025-03-02 20:59:06 +0500

    added dependency to maven, added config file and two components

---
+++ src/main/java/components/Product.java
@@ -0,0 +1,28 @@
+package components;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private double productPrice;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price) {
+        this.productId = id;
+        this.productName = name;
+        this.productPrice = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+}
